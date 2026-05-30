2367ed6dcefff7a24fb092dc69f4bef0f9e09263
wjsimm9520 <wjsimm9520@gmail.com>
2026-05-29 21:16:57 -0400

    Commit Finished Configuring Authentication and Identity

--- BookStoreApp.API/Data/BookStoreDbContext.cs
+++ BookStoreApp.API/Data/BookStoreDbContext.cs
@@ -1,57 +1,129 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 
-namespace BookStoreApp.API.Data;
-
-public partial class BookStoreDbContext : DbContext
+namespace BookStoreApp.API.Data
 {
-    public BookStoreDbContext()
+    public partial class BookStoreDbContext : IdentityDbContext<ApiUser>
     {
-    }
-
-    public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options)
-        : base(options)
-    {
-    }
-
-    public virtual DbSet<Author> Authors { get; set; } = null!;
-
-    public virtual DbSet<Book> Books { get; set; } = null!;
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Author>(entity =>
+        public BookStoreDbContext()
         {
-            entity.Property(e => e.Bio).HasMaxLength(250);
+        }
 
-            entity.Property(e => e.FirstName).HasMaxLength(50);
-
-            entity.Property(e => e.LastName).HasMaxLength(50);
-        });
-
-        modelBuilder.Entity<Book>(entity =>
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options)
+            : base(options)
         {
-            entity.HasIndex(e => e.Isbn, "UQ__Books__447D36EA158F90D7").IsUnique();
+        }
 
-            entity.Property(e => e.Image).HasMaxLength(50);
-            entity.Property(e => e.Isbn)
-                .HasMaxLength(50)
-                .HasColumnName("ISBN");
+        public virtual DbSet<Author> Authors { get; set; } = null!;
 
-            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+        public virtual DbSet<Book> Books { get; set; } = null!;
 
-            entity.Property(e => e.Summary).HasMaxLength(250);
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
 
-            entity.Property(e => e.Title).HasMaxLength(50);
+            // Suppresses the strict non-deterministic model check warning
+            optionsBuilder.ConfigureWarnings(warnings =>
+                warnings.Ignore(Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.PendingModelChangesWarning));
+        }
 
-            entity.HasOne(d => d.Author).WithMany(p => p.Books)
-                .HasForeignKey(d => d.AuthorId)
-                .HasConstraintName("FK_Books_ToTable");
-        });
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
-        OnModelCreatingPartial(modelBuilder);
+            modelBuilder.Entity<Author>(entity =>
+            {
+                entity.Property(e => e.Bio).HasMaxLength(250);
+
+                entity.Property(e => e.FirstName).HasMaxLength(50);
+
+                entity.Property(e => e.LastName).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.HasIndex(e => e.Isbn, "UQ__Books__447D36EA158F90D7").IsUnique();
+
+                entity.Property(e => e.Image).HasMaxLength(250);
+
+                entity.Property(e => e.Isbn)
+                    .HasMaxLength(50)
+                    .HasColumnName("ISBN");
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+
+                entity.Property(e => e.Summary).HasMaxLength(250);
+
+                entity.Property(e => e.Title).HasMaxLength(50);
+
+                entity.HasOne(d => d.Author).WithMany(p => p.Books)
+                    .HasForeignKey(d => d.AuthorId)
+                    .HasConstraintName("FK_Books_ToTable");
+            });
+
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER",
+                    Id = "1d9d71ed-e29a-40c1-82ff-d6c8440daa26"
+                },
+                new IdentityRole
+                {
+                Name = "Administrator",
+                    NormalizedName = "ADMINISTRATOR",
+                    Id = "9fc78a51-c80e-43ad-b26e-c7bb709f14e8"
+                }
+            );
+
+            var hasher = new PasswordHasher<ApiUser>();
+
+            modelBuilder.Entity<ApiUser>().HasData(
+                new ApiUser
+                {
+                    Id = "a8d47df2-90dc-4df0-af1e-2b9cf2189c6c",
+                    Email = "admin@bookstor.com",
+                    NormalizedEmail = "ADMIN@BOOKSTORE.COM",
+                    UserName = "admin@bookstor.com",
+                    NormalizedUserName = "ADMIN@BOOKSTORE.COM",
+                    FirstName = "System",
+                    LastName = "Admin",
+                    PasswordHash = "AQAAAAIAAYagAAAAEJa0exZEOzCjw2IcipBDYiG7NNvBmFJFy91bDMo9Y+u7Vjhu9uSVnr+JkdLZnD5NtQ=="
+                },
+                new ApiUser
+                {
+                    Id = "5f1136f4-93a8-4bbf-9018-b70a90486d5a",
+                    Email = "user@bookstor.com",
+                    NormalizedEmail = "USER@BOOKSTORE.COM",
+                    UserName = "user@bookstor.com",
+                    NormalizedUserName = "USER@BOOKSTORE.COM",
+                    FirstName = "System",
+                    LastName = "User",
+                    PasswordHash = "AQAAAAIAAYagAAAAEPTh/tw0XmfRJNR5hhOQLp1gvfyAPt9Ert3J8G9iRrk9LVxqVYDXtTmP24AN0pmkhg=="
+                }
+            );
+
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    RoleId = "1d9d71ed-e29a-40c1-82ff-d6c8440daa26",
+                    UserId = "5f1136f4-93a8-4bbf-9018-b70a90486d5a"
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = "9fc78a51-c80e-43ad-b26e-c7bb709f14e8",
+                    UserId = "a8d47df2-90dc-4df0-af1e-2b9cf2189c6c"
+                }
+
+            );
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
---
+++ BookStoreApp.API/Migrations/20260529172908_SeededDefaultUserandRoles.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStoreApp.API.Migrations
+{
+    public partial class SeededDefaultUserandRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1d9d71ed-e29a-40c1-82ff-d6c8440daa26", "45a961aa-8aea-4584-bf49-cc2f590f84c4", "User", "USER" },
+                    { "9fc78a51-c80e-43ad-b26e-c7bb709f14e8", "2532b09e-98b9-460a-86d9-47def6083b71", "Administrator", "ADMINISTRATOR" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[,]
+                {
+                    { "5f1136f4-93a8-4bbf-9018-b70a90486d5a", 0, "133766b7-d8ce-45de-98e9-d94940f3d52d", "user@bookstor.com", false, "System", "User", false, null, "USER@BOOKSTORE.COM", "USER@BOOKSTORE.COM", "AQAAAAIAAYagAAAAEPTh/tw0XmfRJNR5hhOQLp1gvfyAPt9Ert3J8G9iRrk9LVxqVYDXtTmP24AN0pmkhg==", null, false, "62d2a2d8-db68-4d9c-b798-9dbf00a25c6d", false, "user@bookstor.com" },
+                    { "a8d47df2-90dc-4df0-af1e-2b9cf2189c6c", 0, "965c2abe-462b-4ea2-9fb6-6f162aac7d6e", "admin@bookstor.com", false, "System", "Admin", false, null, "ADMIN@BOOKSTORE.COM", "ADMIN@BOOKSTORE.COM", "AQAAAAIAAYagAAAAEJa0exZEOzCjw2IcipBDYiG7NNvBmFJFy91bDMo9Y+u7Vjhu9uSVnr+JkdLZnD5NtQ==", null, false, "168d2b51-4462-4f97-a799-f24df7716eff", false, "admin@bookstor.com" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "1d9d71ed-e29a-40c1-82ff-d6c8440daa26", "5f1136f4-93a8-4bbf-9018-b70a90486d5a" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "9fc78a51-c80e-43ad-b26e-c7bb709f14e8", "a8d47df2-90dc-4df0-af1e-2b9cf2189c6c" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "1d9d71ed-e29a-40c1-82ff-d6c8440daa26", "5f1136f4-93a8-4bbf-9018-b70a90486d5a" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "9fc78a51-c80e-43ad-b26e-c7bb709f14e8", "a8d47df2-90dc-4df0-af1e-2b9cf2189c6c" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1d9d71ed-e29a-40c1-82ff-d6c8440daa26");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "9fc78a51-c80e-43ad-b26e-c7bb709f14e8");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "5f1136f4-93a8-4bbf-9018-b70a90486d5a");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a8d47df2-90dc-4df0-af1e-2b9cf2189c6c");
+        }
+    }
+}
